64ebb17d07c91c7d7109d4eb48283335d44dd43d
Irine Tuzova <alexandrinius@outlook.com>
2021-09-04 19:39:27 +0300

    Initial commit

---
+++ src/test/java/ru/netology/domain/BookTest.java
@@ -0,0 +1,26 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+    @Test
+    public void shouldHaveAllFieldsAndMethodFromSuperClass() {
+        Book book = new Book();
+    }
+
+    @Test
+    public void shouldCastFromBaseClass() {
+        Product product = new Book();
+        if (product instanceof Book) {
+            Book book = (Book) product;
+        }
+    }
+
+    @Test
+    public void shouldUseOverridedMethod() {
+        Product product = new Book();
+        product.toString();
+    }
+}
---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,54 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Book;
+import ru.netology.domain.Smartphone;
+import ru.netology.domain.Product;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ProductRepositoryTest {
+    private ProductRepository repository = new ProductRepository();
+    private Book Dubliners = new Book(124, "Dubliners", 1000, "Joyce");
+    private Smartphone iPhoneSE20 = new Smartphone(1003, "iPhoneSE20", 44900, "Apple");
+
+    @Test
+    public void shouldSaveOneBook() {
+        repository.save(Dubliners);
+
+        Product[] expected = new Product[]{Dubliners};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSaveOneSmartphone() {
+        repository.save(iPhoneSE20);
+
+        Product[] expected = new Product[]{iPhoneSE20};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        repository.save(Dubliners);
+        repository.save(iPhoneSE20);
+        repository.removeById(1003);
+
+        Product[] expected = new Product[]{Dubliners};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowNotFoundException() {
+        repository.save(Dubliners);
+        repository.save(iPhoneSE20);
+
+        assertThrows(NotFoundException.class, () -> {
+            repository.removeById(1000);
+        });
+    }
+}
+
